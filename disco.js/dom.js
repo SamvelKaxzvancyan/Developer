3e151e994803d4ab02cc8b0f9bb46297da34252c
Samvel <123316366+SamvelKaxzvancyan@users.noreply.github.com>
2023-04-02 20:47:50 +0400

    Add files via upload

--- disco.js/dom.js
+++ disco.js/dom.js
@@ -1,1 +1,18 @@
+const arr = [1, 3,5]
+const rowIndex = [1, 3, 5, 7, 9, 11];
+
+let color1 = ("");
+
+function update(){
+    for (let i = 0; i < arr.length; i++){
+        for(let index = 0; index < rowIndex.length; index++){
+        color1 = `rgb(${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)})`
+        document.body.firstElementChild.childNodes[arr[i]].childNodes[rowIndex[index]].style.background = color1;
+        console.log(color1);
+        }
+    }
+}
+setInterval(()=> {
+    update();
+}, 50)
 
